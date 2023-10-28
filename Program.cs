023aff0f862eb04fc7c8f2c7f53d4be0795b2c59
Victor <victorrefundine@hotmail.com>
2023-10-27 22:13:07 -0300

    Adição do programa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("MÚLTIPLO\n");
+
+Console.Write("Digite um número: ");
+int numeroUm = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Avaliar se é múltiplo de: ");
+int numeroDois = Convert.ToInt32(Console.ReadLine());
+
+if (numeroUm % numeroDois == 0)
+{
+    Console.WriteLine($"\nO {numeroUm} é multiplo de {numeroDois}.");
+}
+else
+{
+    Console.WriteLine($"\nO {numeroUm} não é multiplo de {numeroDois}.");
+}
